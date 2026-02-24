155b011ada3031d4356ae40975113005d74624d9
achbj <bj.ach7@gmail.com>
2026-02-24 18:47:59 +0100

    chore: Bump package version to 0.2.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bonza-mragent",
-    version="0.2.1",
+    version="0.2.2.",
     description="A lightweight, open-source AI Agent powered by free APIs",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
